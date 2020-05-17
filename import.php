02800dc40815960dede94042875466415b9b5838
sim <simotrone@gmail.com>
2020-05-17 20:54:15 +0200

    more log

--- import.php
+++ import.php
@@ -22,25 +22,40 @@
 
                 if (is_array($imports)) {
                     foreach ($imports as $import) {
-                        if (Utils::endswith($import, '.yaml')) {
-                            $key = basename($import, '.yaml');
-                            $parsed[$key] = Yaml::parse($this->getContents($import));
-                        } elseif (Utils::endswith($import, '.json')) {
-                            $key = basename($import, '.json');
-                            $parsed[$key] = json_decode($this->getContents($import), true);
-                        }
+                        $res = $this->decodeFile($import);
+                        $parsed[$res['basename']] = $res['decoded'];
                     }
                 } else {
-                    $import = $imports;
-                    if (Utils::endswith($import, '.yaml')) {
-                        $parsed = Yaml::parse($this->getContents($import));
-                    } elseif (Utils::endswith($import, '.json')) {
-                        $parsed = json_decode($this->getContents($import), true);
-                    }
+                    $res = $this->decodeFile($imports);
+                    $parsed = $res['decoded'];
                 }
 
                 $this->grav['page']->header()->imports = $parsed;
             }
+        }
+
+        private function decodeFile($sourceFile) {
+            $parts = pathinfo($sourceFile);
+            $ret = array(
+                'basename' => $parts['filename'],
+                'decoded' => null,
+            );
+
+            $content = $this->getContents($sourceFile);
+            if (!$content) {
+                $this->grav['log']->error("$this->name | Cannot read '$sourceFile' file");
+                return $ret;
+            }
+
+            $ext = $parts['extension'];
+            if ($ext == 'yaml') {
+                $ret['decoded'] = YAML::parse($content);
+            } elseif ($ext == 'json') {
+                $ret['decoded'] = json_decode($content, true);
+            } else {
+                $this->grav['log']->alert("$this->name | The extension '$ext' from file '$sourceFile' is not managed.");
+            }
+            return $ret;
         }
 
         private function getContents($fn) {
